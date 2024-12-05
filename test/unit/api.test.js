b2e4e886ad173b337254fbf29ec3171137c564d4
jonafiction <alva.jonathan@hotmail.com>
2024-12-04 21:43:12 -0300

    delete test

--- test/unit/api.test.js
+++ test/unit/api.test.js
@@ -1,14 +1,13 @@
-const request = require('supertest');
-const app = require('../../src/app');
+const request = require("supertest");
+const app = require("../../src/app");
 
 test('La ruta "/" responde con un mensaje', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('¡Hola, mundo desde Node.js!');
+  const response = await request(app).get("/");
+  expect(response.statusCode).toBe(200);
 });
 
 test('La ruta "/api" responde con datos de la API externa', async () => {
-    const response = await request(app).get('/api');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty('id', 1);
+  const response = await request(app).get("/api");
+  expect(response.statusCode).toBe(200);
+  expect(response.body).toHaveProperty("id", 1);
 });
