45e25f8496306514cfff95a8511db74511a5cd39
jonafiction <alva.jonathan@hotmail.com>
2024-12-04 21:40:33 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo desde Node.js!');
+app.get("/", (req, res) => {
+  res.send({ MESSAGE: "Hola desde node en Docker" });
 });
 
-app.get('/api', async (req, res) => {
-    try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).send('Error al obtener datos de la API externa');
-    }
+app.get("/api", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts/1"
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send("Error al obtener datos de la API externa");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
 
 module.exports = app;
